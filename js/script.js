76235e1949066a2483150d5788b84e681a975086
polu <example@gmail.com>
2021-01-12 20:52:32 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+
+
+// let answer = +prompt("est vam 18", "Yes");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + "- object");
+// console.log(4 + " - object" );
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(decr--);
+console.log(decr--)
