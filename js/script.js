ad99f61f9caf9052a2853ffdfee6b585c55d5c90
polu <example@gmail.com>
2021-01-13 21:36:53 +0200

    13.01.2021

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
 'use strict'
 
-let money=prompt("Ваш бюджет на месяц?");
-let time=prompt("Введите дату в формате YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let appData={
-    moneys: money,
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     timeDate: time,
-    expenses:{},
-    optionalExpenses:{},
-    income:[],
-    savings:false
+    savings: false
 };
 
-let a1=prompt("Введите обязательную статью расходов в этом месяце"),
-    a2=prompt("Во сколько обойдется?"),
-    a3=prompt("Введите обязательную статью расходов в этом месяце"),
-    a4=prompt("Во сколько обойдется?");
 
-appData.expenses.a1 = a3;
-appData.expenses.a2 = a4;
 
-console.log(appData.moneys/30);
+for(let i =0; i<2;i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
+
+    if((typeof(a))==='string' && (typeof(a))!=null && (typeof(b))!=null && a!='' && b!='' ){
+        console.log('done');
+        appData.expenses[a] = b;
+    }else{
+        console.log("bad result");
+        i--;
+    }
+
+    
+    
+}
+appData.moneyPerDay=appData.budget/30;
+
+alert("Бюджет на деньЖ "+appData.moneyPerDay);
+
+if(appData.moneyPerDay<100){
+    console.log("min urov doctatka");
+}else if (appData.moneyPerDay>100 &&appData.moneyPerDay<2000){
+    console.log("sredn urov doctatka");
+}else if (appData.moneyPerDay>2000){
+    console.log("vysok urov doctatka");
+}else{
+    console.log("Error!!!")
+}
---
+++ proba Pera.js
@@ -0,0 +1,19 @@
+let num = 1;
+
+// while(num<55){
+//     console.log(num);
+//     num++;
+// }
+
+// do{
+//     console.log(num);
+//     num++;
+// }
+// while(num<55);
+
+for(let i=1; i<8; i++){
+    if(i==3){
+        break
+    }
+    console.log(i);
+}
