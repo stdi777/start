a5d5bc8089341e63dba3a95d4e9fb5202d35261c
polu <example@gmail.com>
2021-01-13 20:32:41 +0200

    three

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 'use strict'
 
+let money=prompt("Ваш бюджет на месяц?");
+let time=prompt("Введите дату в формате YYYY-MM-DD");
 
+let appData={
+    moneys: money,
+    timeDate: time,
+    expenses:{},
+    optionalExpenses:{},
+    income:[],
+    savings:false
+};
 
+let a1=prompt("Введите обязательную статью расходов в этом месяце"),
+    a2=prompt("Во сколько обойдется?"),
+    a3=prompt("Введите обязательную статью расходов в этом месяце"),
+    a4=prompt("Во сколько обойдется?");
 
-// let answer = +prompt("est vam 18", "Yes");
+appData.expenses.a1 = a3;
+appData.expenses.a2 = a4;
 
-// console.log(typeof(answer));
-
-// console.log("arr" + "- object");
-// console.log(4 + " - object" );
-
-let incr = 10,
-    decr = 10;
-
-
-console.log(++incr);
-console.log(decr--);
-console.log(decr--)
+console.log(appData.moneys/30);
